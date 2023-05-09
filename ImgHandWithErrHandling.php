5bd53a342da1f0fc0ed078747ed9464a03408c68
DevilsNerve <austengreenj@gmail.com>
2023-05-09 11:54:21 -0500

    Update ImgHandWithErrHandling.php

--- ImgHandWithErrHandling.php
+++ ImgHandWithErrHandling.php
@@ -1,12 +1,10 @@
 <?php
-class ImageHandler
-{
+class ImageHandler {
     private $image;
     private $imageType;
 
-    function load($filename) 
-    {
-        if(!file_exists($filename)) {
+    function load($filename) {
+        if (!file_exists($filename)) {
             throw new Exception('File not found: ' . $filename);
         }
 
@@ -27,54 +25,105 @@ class ImageHandler
             throw new Exception('Unsupported image type');
         }
 
-        if($this->image === false) {
+        if ($this->image === false) {
             throw new Exception('Image creation failed');
         }
     }
 
-    function save($filename, $imageType = IMAGETYPE_JPEG, $compression = 75, $permissions = null) 
-    {
+    function save($filename, $imageType = IMAGETYPE_JPEG, $compression = 75, $permissions = null) {
         if ($imageType == IMAGETYPE_JPEG) {
-            if(!imagejpeg($this->image, $filename, $compression)) {
+            if (!imagejpeg($this->image, $filename, $compression)) {
                 throw new Exception('Saving JPEG image failed');
             }
         } elseif ($imageType == IMAGETYPE_GIF) {
-            if(!imagegif($this->image, $filename)) {
+            if (!imagegif($this->image, $filename)) {
                 throw new Exception('Saving GIF image failed');
             }
         } elseif ($imageType == IMAGETYPE_PNG) {
-            if(!imagepng($this->image, $filename)) {
+            if (!imagepng($this->image, $filename)) {
                 throw new Exception('Saving PNG image failed');
             }
         }
 
         if ($permissions != null) {
-            if(!chmod($filename, $permissions)) {
+            if (!chmod($filename, $permissions)) {
                 throw new Exception('Setting image permissions failed');
             }
         }
     }
 
-    // More functions...
+    function resizeToHeight($height) {
+        if (!is_numeric($height) || $height <= 0) {
+            throw new Exception('Invalid height value. It must be a positive numeric value.');
+        }
 
-    function resize($width, $height) 
-    {
+        $ratio = $height / $this->getHeight();
+        $width = $this->getWidth() * $ratio;
+        $this->resize($width, $height);
+    }
+
+    function resizeToWidth($width) {
+        if (!is_numeric($width) || $width <= 0) {
+            throw new Exception('Invalid width value. It must be a positive numeric value.');
+        }
+
+        $ratio = $width / $this->getWidth();
+        $height = $this->getHeight() * $ratio;
+        $this->resize($width, $height);
+    }
+
+    function scale($scale) {
+        if (!is_numeric($scale) || $scale <= 0 || $scale > 100) {
+            throw new Exception('Invalid scale value. It must be a positive numeric value between 1 and 100.');
+        }
+
+        $width = $this->getWidth() * $scale / 100;
+        $height = $this->getHeight() * $scale / 100;
+        $this->resize($width, $height);
+    }
+
+    function resize($width, $height) {
+        if (!is_numeric($width) || $width <= 0 || !is_numeric($height) || $height <= 0) {
+            throw new Exception('Invalid width or height value. They must be positive numeric values.');
+        }
+
         $newImage = imagecreatetruecolor($width, $height);
-        if($newImage === false) {
+        imagecopyresampled($newImage, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+        $this->image = $newImage;
+    }
+
+    function getWidth() {
+        $width = imagesx($this->image);
+        if ($width === false) {
+            throw new Exception('Failed to retrieve image width.');
+        }
+        return $width;
+    }
+
+    function getHeight() {
+        $height = imagesy($this->image);
+        if ($height === false) {
+            throw new Exception('Failed to retrieve image height.');
+        }
+        return $height;
+    }
+
+
+    function resize($width, $height) {
+        $newImage = imagecreatetruecolor($width, $height);
+        if ($newImage === false) {
             throw new Exception('Image resizing failed');
         }
-        if(!imagecopyresampled($newImage, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight())) {
+        if (!imagecopyresampled($newImage, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight())) {
             throw new Exception('Image copy and resize failed');
         }
         $this->image = $newImage;
     }
 
-    function convertToWebP($filename) 
-    {
-        if(!imagewebp($this->image, $filename)) {
+    function convertToWebP($filename) {
+        if (!imagewebp($this->image, $filename)) {
             throw new Exception('Image conversion to WebP failed');
         }
     }
 
-    // More functions...
 }
